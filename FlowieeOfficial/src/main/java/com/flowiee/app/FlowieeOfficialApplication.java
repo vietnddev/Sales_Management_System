b2a7d6a6c2c0d6c6c0cc7af877edbb17979c6939
vietnddev <124055768+vietnddev@users.noreply.github.com>
2023-02-13 11:39:56 +0700

    Add files via upload

---
+++ FlowieeOfficial/src/main/java/com/flowiee/app/FlowieeOfficialApplication.java
@@ -0,0 +1,13 @@
+package com.flowiee.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowieeOfficialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowieeOfficialApplication.class, args);
+	}
+
+}
---
+++ FlowieeOfficial/src/test/java/com/flowiee/app/FlowieeOfficialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowiee.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowieeOfficialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
