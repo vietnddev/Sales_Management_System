114ac98c77acf49581c9023bf32364baafd2b693
viet0 <nguyenducviet.vietnd@gmail.com>
2023-02-14 22:53:23 +0700

    Update services

--- FlowieeOfficial/src/main/java/com/flowiee/app/services/AccountService.java
+++ FlowieeOfficial/src/main/java/com/flowiee/app/services/AccountService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AccountService {
@@ -14,5 +15,21 @@ public class AccountService {
 
     public List<Account> getAllAccount(){
         return accountRepository.findAll();
+    }
+
+    public Optional<Account> getAccountByID(int ID){
+        return Optional.of(accountRepository.getById(ID));
+    }
+
+    public void deleteAccountByID(int ID){
+        Account account = accountRepository.findById(ID).orElse(null);
+        if (account != null) {
+            accountRepository.delete(account);
+            System.out.println("Successfully deleted the account with id: " + ID);
+        }
+    }
+
+    public void updateOrSave(){
+
     }
 }
