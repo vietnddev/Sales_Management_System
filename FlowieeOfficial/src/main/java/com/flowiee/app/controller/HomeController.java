61e0eb2260fb5b44e46192344754aeb65fd5b0c0
viet0 <nguyenducviet.vietnd@gmail.com>
2023-02-14 15:09:31 +0700

    Commit

--- FlowieeOfficial/src/main/java/com/flowiee/app/controller/HomeController.java
+++ FlowieeOfficial/src/main/java/com/flowiee/app/controller/HomeController.java
@@ -1,5 +1,14 @@
 package com.flowiee.app.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
 
+    @RequestMapping(value = "/")
+    public String homePage() {
+
+        return "home";
+    }
 }
