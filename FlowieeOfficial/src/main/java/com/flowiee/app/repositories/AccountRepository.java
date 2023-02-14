9af7b1561523a910bed47140ad41a529f24d68a1
viet0 <nguyenducviet.vietnd@gmail.com>
2023-02-14 14:58:11 +0700

    Commit

--- FlowieeOfficial/src/main/java/com/flowiee/app/repositories/AccountRepository.java
+++ FlowieeOfficial/src/main/java/com/flowiee/app/repositories/AccountRepository.java
@@ -1,5 +1,8 @@
 package com.flowiee.app.repositories;
 
-public class AccountRepository {
+import com.flowiee.app.model.admin.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Integer> {
 
 }
