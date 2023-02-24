31b9582f91208c09f40f1b20d09c653381beccdc
viet0 <nguyenducviet.vietnd@gmail.com>
2023-02-24 15:28:04 +0700

    Upload image of product

--- FlowieeOfficial/src/main/java/com/flowiee/app/controller/sales/ProductController.java
+++ FlowieeOfficial/src/main/java/com/flowiee/app/controller/sales/ProductController.java
@@ -12,6 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 @RequestMapping(path = "/sales/product")
@@ -62,6 +70,46 @@ public class ProductController {
     public String DeleteProruct(@PathVariable int id) {
         productService.deleteProruct(id);
         return "redirect:/sales/product";
+    }
+
+    @PostMapping(value = "/upload2")
+    public String uploadImage(@RequestParam("file") MultipartFile file, HttpServletRequest request) throws IOException {
+        Path path;
+        String rootDir = request.getSession().getServletContext().getRealPath("/");
+        if (file != null && !file.isEmpty()) {
+            path = Paths.get("E:\\Test\\abc.png");
+            file.transferTo(new File(path.toString()));
+        }
+        System.out.println("Ok upload");
+        return "redirect:" + request.getHeader("referer");
+    }
+
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file, HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        // Kiểm tra nếu file không được chọn
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Vui lòng chọn file để upload");
+            return "redirect://sales/product/upload";
+        }
+
+        try {
+            // Lưu file vào thư mục tạm thời
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get("E:\\Test\\" + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            // Lưu file vào database
+//            Product product = new Product();
+//            product.setImage(bytes);
+//            productRepository.save(product);
+
+            redirectAttributes.addFlashAttribute("message",
+                    "Upload file thành công '" + file.getOriginalFilename() + "'");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            return "redirect:" + request.getHeader("referer");
+        }
     }
 
     @GetMapping(value = "/sync")
