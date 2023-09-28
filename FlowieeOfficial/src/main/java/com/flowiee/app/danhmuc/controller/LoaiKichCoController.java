dea8ff934b5653fb80560360f986ce1ac5931faf
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-09-28 17:02:39 +0700

    Export data danh mục kích cỡ

--- FlowieeOfficial/src/main/java/com/flowiee/app/danhmuc/controller/LoaiKichCoController.java
+++ FlowieeOfficial/src/main/java/com/flowiee/app/danhmuc/controller/LoaiKichCoController.java
@@ -2,16 +2,23 @@ package com.flowiee.app.danhmuc.controller;
 
 import com.flowiee.app.common.authorization.KiemTraQuyenModuleDanhMuc;
 import com.flowiee.app.common.exception.BadRequestException;
+import com.flowiee.app.common.utils.FileUtil;
 import com.flowiee.app.common.utils.PagesUtil;
 import com.flowiee.app.danhmuc.entity.LoaiKichCo;
 import com.flowiee.app.danhmuc.service.LoaiKichCoService;
 import com.flowiee.app.hethong.service.AccountService;
 import com.flowiee.app.hethong.service.SystemLogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -80,5 +87,21 @@ public class LoaiKichCoController {
         }
         loaiKichCoService.delete(id);
         return "redirect:" + request.getHeader("referer");
+    }
+
+    @GetMapping("/export")
+    public ResponseEntity<?> exportData() {
+        if (!accountService.isLogin()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(PagesUtil.PAGE_LOGIN);
+        }
+        if (kiemTraQuyenModule.kiemTraQuyenExport()) {
+            byte[] dataExport = loaiKichCoService.exportData();
+            HttpHeaders header = new HttpHeaders();
+            header.setContentType(new MediaType("application", "force-download"));
+            header.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + FileUtil.TEMPLATE_DM_LOAIKICHCO + ".xlsx");
+            return new ResponseEntity<>(new ByteArrayResource(dataExport), header, HttpStatus.CREATED);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(PagesUtil.PAGE_UNAUTHORIZED);
+        }
     }
 }
--- FlowieeOfficial/src/main/java/com/flowiee/app/danhmuc/service/impl/LoaiKichCoServiceImpl.java
+++ FlowieeOfficial/src/main/java/com/flowiee/app/danhmuc/service/impl/LoaiKichCoServiceImpl.java
@@ -1,13 +1,32 @@
 package com.flowiee.app.danhmuc.service.impl;
 
 import com.flowiee.app.common.exception.NotFoundException;
+import com.flowiee.app.common.utils.ExcelUtil;
+import com.flowiee.app.common.utils.FileUtil;
+import com.flowiee.app.common.utils.FlowieeUtil;
 import com.flowiee.app.danhmuc.entity.LoaiKichCo;
 import com.flowiee.app.danhmuc.repository.LoaiKichCoRepository;
 import com.flowiee.app.danhmuc.service.LoaiKichCoService;
+import com.flowiee.app.sanpham.entity.DonHang;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.time.Clock;
+import java.time.Instant;
 import java.util.List;
 
 @Service
@@ -72,6 +91,35 @@ public class LoaiKichCoServiceImpl implements LoaiKichCoService {
 
     @Override
     public byte[] exportData() {
-        return new byte[0];
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        String filePathOriginal = FlowieeUtil.PATH_TEMPLATE_EXCEL + "/" + FileUtil.TEMPLATE_DM_LOAIKICHCO + ".xlsx";
+        String filePathTemp = FlowieeUtil.PATH_TEMPLATE_EXCEL + "/" + FileUtil.TEMPLATE_DM_LOAIKICHCO + "_" + Instant.now(Clock.systemUTC()).toEpochMilli() + ".xlsx";
+        File fileDeleteAfterExport = new File(Path.of(filePathTemp).toUri());
+        try {
+            XSSFWorkbook workbook = new XSSFWorkbook(Files.copy(Path.of(filePathOriginal),
+                                                     Path.of(filePathTemp),
+                                                     StandardCopyOption.REPLACE_EXISTING).toFile());
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            List<LoaiKichCo> listData = this.findAll();
+            for (int i = 0; i < listData.size(); i++) {
+                XSSFRow row = sheet.createRow(i + 3);
+                row.createCell(0).setCellValue(i + 1);
+                row.createCell(1).setCellValue(listData.get(i).getMaLoai());
+                row.createCell(2).setCellValue(listData.get(i).getTenLoai());
+                row.createCell(3).setCellValue(listData.get(i).getGhiChu());
+                for (int j = 0; j <= 3; j++) {
+                    row.getCell(j).setCellStyle(ExcelUtil.setBorder(workbook.createCellStyle()));
+                }
+            }
+            workbook.write(stream);
+            workbook.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fileDeleteAfterExport.exists()) {
+                fileDeleteAfterExport.delete();
+            }
+        }
+        return stream.toByteArray();
     }
 }
