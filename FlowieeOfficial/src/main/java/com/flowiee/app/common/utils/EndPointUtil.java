84e53c28a7c74e7c51e3bf485dbe06014dac7294
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-09-30 12:51:21 +0700

    Add EndPointsUtil

---
+++ FlowieeOfficial/src/main/java/com/flowiee/app/common/utils/EndPointUtil.java
@@ -0,0 +1,13 @@
+package com.flowiee.app.common.utils;
+
+public class EndPointUtil {
+    public static String SAN_PHAM_READ = "/san-pham";
+    public static String SAN_PHAM_CREATE = "/san-pham/insert";
+    public static String ACCOUNT_READ = "/he-thong/tai-khoan";
+    public static String ACCOUNT_CREATE = "/he-thong/tai-khoan/insert";
+    public static String KHACH_HANG_READ = "/khach-hang";
+    public static String KHACH_HANG_CREATE = "/khach-hang/create";
+    public static String LOG_READ = "/he-thong/nhat-ky";
+    public static String NOTIFICATION_READ = "/he-thong/notification";
+    public static String CONFIG_READ = "/he-thong/config";
+}
