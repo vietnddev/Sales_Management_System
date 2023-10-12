b9afee2525cbac8de8478bf99f8d04dd9ef9c228
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-10-12 17:38:03 +0700

    Add NotificationUtil

---
+++ FlowieeOfficial/src/main/java/com/flowiee/app/common/utils/NotificationUtil.java
@@ -0,0 +1,20 @@
+package com.flowiee.app.common.utils;
+
+public class NotificationUtil {
+    public static final String NOTI_TYPE_IMPORT = "IMPORT";
+
+    public static final String IMPORT_DM_DONVITINH_SUCCESS = "Import dữ liệu vào danh mục Đơn vị tính THÀNH CÔNG!";
+    public static final String IMPORT_DM_DONVITINH_FAIL = "Import dữ liệu vào danh mục Đơn vị tính THẤT BẠI!";
+    public static final String IMPORT_DM_HINHTHUCTHANHTOAN_SUCCESS = "Import dữ liệu vào danh mục Hình thức thanh toán THÀNH CÔNG!";
+    public static final String IMPORT_DM_HINHTHUCTHANHTOAN_FAIL = "Import dữ liệu vào danh mục Hình thức thanh toán THẤT BẠI!";
+    public static final String IMPORT_DM_KENHBANHANG_SUCCESS = "Import dữ liệu vào danh mục Kênh bán hàng THÀNH CÔNG!";
+    public static final String IMPORT_DM_KENHBANHANG_FAIL = "Import dữ liệu vào danh mục Kênh bán hàng THẤT BẠI!";
+    public static final String IMPORT_DM_LOAIKICHCO_SUCCESS = "Import dữ liệu vào danh mục Loại kích cỡ THÀNH CÔNG!";
+    public static final String IMPORT_DM_LOAIKICHCO_FAIL = "Import dữ liệu vào danh mục Loại kích cỡ THẤT BẠI!";
+    public static final String IMPORT_DM_LOAIMAUSAC_SUCCESS = "Import dữ liệu vào danh mục Loại màu sắc THÀNH CÔNG!";
+    public static final String IMPORT_DM_LOAIMAUSAC_FAIL = "Import dữ liệu vào danh mục Loại màu sắc THẤT BẠI!";
+    public static final String IMPORT_DM_LOAISANPHAM_SUCCESS = "Import dữ liệu vào danh mục Loại sản phẩm THÀNH CÔNG!";
+    public static final String IMPORT_DM_LOAISANPHAM_FAIL = "Import dữ liệu vào danh mục Loại sản phẩm THẤT BẠI!";
+    public static final String IMPORT_DM_LOAITAILIEU_SUCCESS = "Import dữ liệu vào danh mục Loại tài liệu THÀNH CÔNG!";
+    public static final String IMPORT_DM_LOAITAILIEU_FAIL = "Import dữ liệu vào danh mục Loại tài liệu THẤT BẠI!";
+}
