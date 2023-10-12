b044b91a8a6c9379dc738af6435c81c1d48cb080
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-10-12 09:19:55 +0700

    Update EndPointUtil

--- FlowieeOfficial/src/main/java/com/flowiee/app/common/utils/EndPointUtil.java
+++ FlowieeOfficial/src/main/java/com/flowiee/app/common/utils/EndPointUtil.java
@@ -1,16 +1,89 @@
 package com.flowiee.app.common.utils;
 
 public class EndPointUtil {
-    public static String SAN_PHAM_READ = "/san-pham";
-    public static String SAN_PHAM_CREATE = "/san-pham/insert";
-    public static String ACCOUNT_READ = "/he-thong/tai-khoan";
-    public static String ACCOUNT_CREATE = "/he-thong/tai-khoan/insert";
-    public static String KHACH_HANG_READ = "/khach-hang";
-    public static String KHACH_HANG_CREATE = "/khach-hang/create";
-    public static String LOG_READ = "/he-thong/nhat-ky";
-    public static String NOTIFICATION_READ = "/he-thong/notification";
-    public static String CONFIG_READ = "/he-thong/config";
+    public static String NOTIFICATION_VIEW = "/he-thong/notification";
+    public static String CONFIG_VIEW = "/he-thong/config";
+
+    public static String LOG_EXPORT = "/he-thong/nhat-ky/export";
+    public static String LOG_VIEW = "/he-thong/nhat-ky";
+
+    public static String ACCOUNT_TEMPLATE = "/he-thong/tai-khoan/template";
+    public static String ACCOUNT_IMPORT = "/he-thong/tai-khoan/import";
+    public static String ACCOUNT_EXPORT = "/he-thong/tai-khoan/export";
+    public static String ACCOUNT_VIEW = "/he-thong/tai-khoan";
+    public static String ACCOUNT_INSERT = "/he-thong/tai-khoan/insert";
+
+    public static String SANPHAM_TEMPLATE = "/san-pham/template";
+    public static String SANPHAM_IMPORT = "/san-pham/import";
+    public static String SANPHAM_EXPORT = "/san-pham/export";
+    public static String SANPHAM_VIEW = "/san-pham";
+    public static String SANPHAM_INSERT = "/san-pham/insert";
+
+    public static String DONHANG_TEMPLATE = "/don-hang/template";
+    public static String DONHANG_IMPORT = "/don-hang/import";
+    public static String DONHANG_EXPORT = "/don-hang/export";
+    public static String DONHANG_VIEW = "/don-hang";
+
+    public static String KHACHHANG_TEMPLATE = "/khach-hang/template";
+    public static String KHACHHANG_IMPORT = "/khach-hang/import";
+    public static String KHACHHANG_EXPORT = "/khach-hang/export";
+    public static String KHACHHANG_VIEW = "/khach-hang";
+    public static String KHACHHANG_INSERT = "/khach-hang/create";
+
+    public static String DOCUMENT_EXPORT = "/kho-tai-lieu/export";
+    public static String DOCUMENT_VIEW = "/kho-tai-lieu";
+
     public static String DANHMUC_DONVITINH_TEMPLATE = "/danh-muc/don-vi-tinh/template";
     public static String DANHMUC_DONVITINH_IMPORT = "/danh-muc/don-vi-tinh/import";
     public static String DANHMUC_DONVITINH_EXPORT = "/danh-muc/don-vi-tinh/export";
+    public static String DANHMUC_DONVITINH_INSERT = "/danh-muc/don-vi-tinh/insert";
+    public static String DANHMUC_DONVITINH_VIEW = "/danh-muc/don-vi-tinh";
+
+    public static String DANHMUC_KENHBANHANG_TEMPLATE = "/danh-muc/kenh-ban-hang/template";
+    public static String DANHMUC_KENHBANHANG_IMPORT = "/danh-muc/kenh-ban-hang/import";
+    public static String DANHMUC_KENHBANHANG_EXPORT = "/danh-muc/kenh-ban-hang/export";
+    public static String DANHMUC_KENHBANHANG_INSERT = "/danh-muc/kenh-ban-hang/insert";
+    public static String DANHMUC_KENHBANHANG_VIEW = "/danh-muc/kenh-ban-hang";
+
+    public static String DANHMUC_LOAIMAUSAC_TEMPLATE = "/danh-muc/loai-mau-sac/template";
+    public static String DANHMUC_LOAIMAUSAC_IMPORT = "/danh-muc/loai-mau-sac/import";
+    public static String DANHMUC_LOAIMAUSAC_EXPORT = "/danh-muc/loai-mau-sac/export";
+    public static String DANHMUC_LOAIMAUSAC_INSERT = "/danh-muc/loai-mau-sac/insert";
+    public static String DANHMUC_LOAIMAUSAC_VIEW = "/danh-muc/loai-mau-sac";
+
+    public static String DANHMUC_LOAIKICHCO_TEMPLATE = "/danh-muc/loai-kich-co/template";
+    public static String DANHMUC_LOAIKICHCO_IMPORT = "/danh-muc/loai-kich-co/import";
+    public static String DANHMUC_LOAIKICHCO_EXPORT = "/danh-muc/loai-kich-co/export";
+    public static String DANHMUC_LOAIKICHCO_INSERT = "/danh-muc/loai-kich-co/insert";
+    public static String DANHMUC_LOAIKICHCO_VIEW = "/danh-muc/loai-kich-co";
+
+    public static String DANHMUC_HINHTHUCTHANHTOAN_TEMPLATE = "/danh-muc/hinh-thuc-thanh-toan/template";
+    public static String DANHMUC_HINHTHUCTHANHTOAN_IMPORT = "/danh-muc/hinh-thuc-thanh-toan/import";
+    public static String DANHMUC_HINHTHUCTHANHTOAN_EXPORT = "/danh-muc/hinh-thuc-thanh-toan/export";
+    public static String DANHMUC_HINHTHUCTHANHTOAN_INSERT = "/danh-muc/hinh-thuc-thanh-toan/insert";
+    public static String DANHMUC_HINHTHUCTHANHTOAN_VIEW = "/danh-muc/hinh-thuc-thanh-toan";
+
+    public static String DANHMUC_LOAISANPHAM_TEMPLATE = "/danh-muc/loai-san-pham/template";
+    public static String DANHMUC_LOAISANPHAM_IMPORT = "/danh-muc/loai-san-pham/import";
+    public static String DANHMUC_LOAISANPHAM_EXPORT = "/danh-muc/loai-san-pham/export";
+    public static String DANHMUC_LOAISANPHAM_INSERT = "/danh-muc/loai-san-pham/insert";
+    public static String DANHMUC_LOAISANPHAM_VIEW = "/danh-muc/loai-san-pham";
+
+    public static String DANHMUC_LOAITAILIEU_TEMPLATE = "/danh-muc/loai-tai-lieu/template";
+    public static String DANHMUC_LOAITAILIEU_IMPORT = "/danh-muc/loai-tai-lieu/import";
+    public static String DANHMUC_LOAITAILIEU_EXPORT = "/danh-muc/loai-tai-lieu/export";
+    public static String DANHMUC_LOAITAILIEU_INSERT = "/danh-muc/loai-tai-lieu/insert";
+    public static String DANHMUC_LOAITAILIEU_VIEW = "/danh-muc/loai-tai-lieu";
+
+    public static String DANHMUC_TRANGTHAIDONHANG_TEMPLATE = "/danh-muc/trang-thai-don-hang/template";
+    public static String DANHMUC_TRANGTHAIDONHANG_IMPORT = "/danh-muc/trang-thai-don-hang/import";
+    public static String DANHMUC_TRANGTHAIDONHANG_EXPORT = "/danh-muc/trang-thai-don-hang/export";
+    public static String DANHMUC_TRANGTHAIDONHANG_INSERT = "/danh-muc/trang-thai-don-hang/insert";
+    public static String DANHMUC_TRANGTHAIDONHANG_VIEW = "/danh-muc/trang-thai-don-hang";
+
+    public static String DANHMUC_TRANGTHAIGIAOHANG_TEMPLATE = "/danh-muc/trang-thai-giao-hang/template";
+    public static String DANHMUC_TRANGTHAIGIAOHANG_IMPORT = "/danh-muc/trang-thai-giao-hang/import";
+    public static String DANHMUC_TRANGTHAIGIAOHANG_EXPORT = "/danh-muc/trang-thai-giao-hang/export";
+    public static String DANHMUC_TRANGTHAIGIAOHANG_INSERT = "/danh-muc/trang-thai-giao-hang/insert";
+    public static String DANHMUC_TRANGTHAIGIAOHANG_VIEW = "/danh-muc/trang-thai-giao-hang";
 }
