c153a667a2cbe8ebd24b04079a72a381e4d14c7f
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-10-13 09:54:44 +0700

    Update NotificationUtil

--- FlowieeOfficial/src/main/java/com/flowiee/app/common/utils/NotificationUtil.java
+++ FlowieeOfficial/src/main/java/com/flowiee/app/common/utils/NotificationUtil.java
@@ -2,6 +2,7 @@ package com.flowiee.app.common.utils;
 
 public class NotificationUtil {
     public static final String NOTI_TYPE_IMPORT = "IMPORT";
+    public static final String NOTI_TYPE_EXPORT = "IMPORT";
 
     public static final String IMPORT_DM_DONVITINH_SUCCESS = "Import dữ liệu vào danh mục Đơn vị tính THÀNH CÔNG!";
     public static final String IMPORT_DM_DONVITINH_FAIL = "Import dữ liệu vào danh mục Đơn vị tính THẤT BẠI!";
@@ -17,4 +18,35 @@ public class NotificationUtil {
     public static final String IMPORT_DM_LOAISANPHAM_FAIL = "Import dữ liệu vào danh mục Loại sản phẩm THẤT BẠI!";
     public static final String IMPORT_DM_LOAITAILIEU_SUCCESS = "Import dữ liệu vào danh mục Loại tài liệu THÀNH CÔNG!";
     public static final String IMPORT_DM_LOAITAILIEU_FAIL = "Import dữ liệu vào danh mục Loại tài liệu THẤT BẠI!";
+
+    public static final String EXPORT_DM_DONVITINH_SUCCESS = "Export dữ liệu vào danh mục Đơn vị tính THÀNH CÔNG!";
+    public static final String EXPORT_DM_DONVITINH_FAIL = "Export dữ liệu vào danh mục Đơn vị tính THẤT BẠI!";
+    public static final String EXPORT_DM_HINHTHUCTHANHTOAN_SUCCESS = "Export dữ liệu vào danh mục Hình thức thanh toán THÀNH CÔNG!";
+    public static final String EXPORT_DM_HINHTHUCTHANHTOAN_FAIL = "Export dữ liệu vào danh mục Hình thức thanh toán THẤT BẠI!";
+    public static final String EXPORT_DM_KENHBANHANG_SUCCESS = "Export dữ liệu vào danh mục Kênh bán hàng THÀNH CÔNG!";
+    public static final String EXPORT_DM_KENHBANHANG_FAIL = "Export dữ liệu vào danh mục Kênh bán hàng THẤT BẠI!";
+    public static final String EXPORT_DM_LOAIKICHCO_SUCCESS = "Export dữ liệu vào danh mục Loại kích cỡ THÀNH CÔNG!";
+    public static final String EXPORT_DM_LOAIKICHCO_FAIL = "Export dữ liệu vào danh mục Loại kích cỡ THẤT BẠI!";
+    public static final String EXPORT_DM_LOAIMAUSAC_SUCCESS = "Export dữ liệu vào danh mục Loại màu sắc THÀNH CÔNG!";
+    public static final String EXPORT_DM_LOAIMAUSAC_FAIL = "Export dữ liệu vào danh mục Loại màu sắc THẤT BẠI!";
+    public static final String EXPORT_DM_LOAISANPHAM_SUCCESS = "Export dữ liệu vào danh mục Loại sản phẩm THÀNH CÔNG!";
+    public static final String EXPORT_DM_LOAISANPHAM_FAIL = "Export dữ liệu vào danh mục Loại sản phẩm THẤT BẠI!";
+    public static final String EXPORT_DM_LOAITAILIEU_SUCCESS = "Export dữ liệu vào danh mục Loại tài liệu THÀNH CÔNG!";
+    public static final String EXPORT_DM_LOAITAILIEU_FAIL = "Export dữ liệu vào danh mục Loại tài liệu THẤT BẠI!";
+
+    public static final String INSERT_DATA_SUCCESS = "Thêm mới dữ liệu THÀNH CÔNG!";
+    public static final String INSERT_DATA_FAIL = "Thêm mới dữ liệu THẤT BẠI!";
+    public static final String UPDATE_DATA_SUCCESS = "Cập nhật dữ liệu THÀNH CÔNG!";
+    public static final String UPDATE_DATA_FAIL = "Cập nhật dữ liệu THẤT BẠI!";
+    public static final String DELETE_DATA_SUCCESS = "Xóa dữ liệu THÀNH CÔNG!";
+    public static final String DELETE_DATA_FAIL = "Xóa dữ liệu THẤT BẠI!";
+
+    public static final String DATA_EXISTS = "Dữ liệu đã tồn tại!";
+    public static final String DATA_NOT_FOUND = "Dữ liệu không được tìm thấy!";
+
+    public static final String FILE_NOT_FOUND = "File không tồn tại!";
+
+    public static final String RESOURCE_NOT_FOUND = "Tài nguyên không tồn tại!";
+
+    public static final String PAGE_NOT_FOUND = "Trang không tồn tại!";
 }
