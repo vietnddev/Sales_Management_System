1a58708e66f52dd2ef19832049044c1eec8e2ad3
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-10-13 15:03:06 +0700

    Update FlowieeImportRepository

--- FlowieeOfficial/src/main/java/com/flowiee/app/hethong/repository/FlowieeImportRepository.java
+++ FlowieeOfficial/src/main/java/com/flowiee/app/hethong/repository/FlowieeImportRepository.java
@@ -5,10 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface FlowieeImportRepository extends JpaRepository<FlowieeImport, Integer> {
     @Query("from FlowieeImport i where i.account.id=:accountId")
     List<FlowieeImport> findByAccountId(Integer accountId);
+
+    @Query("from FlowieeImport i where i.startTime=:startTime")
+    FlowieeImport findByStartTime(Date startTime);
 }
