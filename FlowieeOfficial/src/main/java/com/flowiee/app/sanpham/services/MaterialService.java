71e0975e91c56fd50c41fca41036e1677886d18a
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-10-20 17:29:10 +0700

    Create MaterialService

---
+++ FlowieeOfficial/src/main/java/com/flowiee/app/sanpham/services/MaterialService.java
@@ -0,0 +1,7 @@
+package com.flowiee.app.sanpham.services;
+
+import com.flowiee.app.base.BaseService;
+import com.flowiee.app.sanpham.entity.Material;
+
+public interface MaterialService extends BaseService<Material> {
+}
