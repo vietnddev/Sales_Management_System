ed3e93b6e02d27f1c76d0af9e5ef90f9e2228479
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-10-20 17:03:35 +0700

    Update FlowieeUtil
    Add method getPaymentStatusCategory() and getOrderStatusCategory()

--- FlowieeOfficial/src/main/java/com/flowiee/app/common/utils/FlowieeUtil.java
+++ FlowieeOfficial/src/main/java/com/flowiee/app/common/utils/FlowieeUtil.java
@@ -4,6 +4,9 @@ import com.flowiee.app.hethong.entity.Account;
 import org.apache.commons.text.StringEscapeUtils;
 
 import java.text.Normalizer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public class FlowieeUtil {
@@ -28,6 +31,23 @@ public class FlowieeUtil {
         }
 
         return result.toString();
+    }
+
+    public static Map<String, String> getOrderStatusCategory() {
+        Map<String, String> deliveryStatus = new HashMap<>();
+        deliveryStatus.put("PREPARING","Đang chuẩn bị");
+        deliveryStatus.put("NOT-DELIVERY","Đang chờ đơn vị vận chuyển tiếp nhận");
+        deliveryStatus.put("DELIVERING","Đang vận chuyển");
+        deliveryStatus.put("COMPLETED","Đã hoàn thành");
+        return deliveryStatus;
+    }
+
+    public static Map<String, String> getPaymentStatusCategory() {
+        Map<String, String> paymentStatus = new HashMap<>();
+        paymentStatus.put("UNPAID","Chưa thanh toán");
+        paymentStatus.put("PARTLY-PAID","Thanh toán một phần");
+        paymentStatus.put("PAID","Đã thanh toán");
+        return paymentStatus;
     }
 
     public static String ACCOUNT_IP = null;
