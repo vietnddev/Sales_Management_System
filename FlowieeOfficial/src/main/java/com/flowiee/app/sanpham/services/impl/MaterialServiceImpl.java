4f7ee8a022a2ac58aa8490036e8ddb21b43d5758
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-10-20 17:29:19 +0700

    Create MaterialServiceImpl

---
+++ FlowieeOfficial/src/main/java/com/flowiee/app/sanpham/services/impl/MaterialServiceImpl.java
@@ -0,0 +1,33 @@
+package com.flowiee.app.sanpham.services.impl;
+
+import com.flowiee.app.sanpham.entity.Material;
+import com.flowiee.app.sanpham.services.MaterialService;
+
+import java.util.List;
+
+public class MaterialServiceImpl implements MaterialService {
+    @Override
+    public List<Material> findAll() {
+        return null;
+    }
+
+    @Override
+    public Material findById(Integer entityId) {
+        return null;
+    }
+
+    @Override
+    public String save(Material entity) {
+        return null;
+    }
+
+    @Override
+    public String update(Material entity, Integer entityId) {
+        return null;
+    }
+
+    @Override
+    public String delete(Integer entityId) {
+        return null;
+    }
+}
