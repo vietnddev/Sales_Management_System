34cd403100e03cb5033ac34fc40c14bce6f641f7
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-10-21 18:05:44 +0700

    Refactor ConfigService extends BaseService

--- FlowieeOfficial/src/main/java/com/flowiee/app/hethong/service/FlowieeConfigService.java
+++ FlowieeOfficial/src/main/java/com/flowiee/app/hethong/service/FlowieeConfigService.java
@@ -1,21 +1,10 @@
 package com.flowiee.app.hethong.service;
 
+import com.flowiee.app.base.BaseService;
 import com.flowiee.app.hethong.entity.FlowieeConfig;
 
-import java.util.List;
-
-public interface FlowieeConfigService {
-    FlowieeConfig findById(Integer id);
-
+public interface FlowieeConfigService extends BaseService<FlowieeConfig> {
     FlowieeConfig findByKey(String key);
-
-    List<FlowieeConfig> findAll();
-
-    String save(FlowieeConfig flowieeConfig);
-
-    String update(FlowieeConfig flowieeConfig, Integer id);
-
-    String delete(Integer id);
 
     void defaultConfig();
 }
