ae0e6797c7d4338eca547a029b63e83a511d027d
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-10-23 17:30:31 +0700

    Create GarmentFactoryService

---
+++ FlowieeOfficial/src/main/java/com/flowiee/app/sanpham/services/GarmentFactoryService.java
@@ -0,0 +1,7 @@
+package com.flowiee.app.sanpham.services;
+
+import com.flowiee.app.base.BaseService;
+import com.flowiee.app.sanpham.entity.GarmentFactory;
+
+public interface GarmentFactoryService extends BaseService<GarmentFactory> {
+}
