6a889da91fc32e33a973af28ec6cd42d67443b1a
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-10-23 17:27:26 +0700

    Update MaterialHistoryRepository
    Add method findByMaterialId(Integer materialId) and findByAction(String action)

--- FlowieeOfficial/src/main/java/com/flowiee/app/sanpham/repository/MaterialHistoryRepository.java
+++ FlowieeOfficial/src/main/java/com/flowiee/app/sanpham/repository/MaterialHistoryRepository.java
@@ -2,8 +2,16 @@ package com.flowiee.app.sanpham.repository;
 
 import com.flowiee.app.sanpham.entity.MaterialHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MaterialHistoryRepository extends JpaRepository<MaterialHistory, Integer> {
+    @Query("from MaterialHistory m where m.material.id=:materialId")
+    List<MaterialHistory> findByMaterialId(Integer materialId);
+
+    @Query("from MaterialHistory m where m.action=:action")
+    List<MaterialHistory> findByAction(String action);
 }
