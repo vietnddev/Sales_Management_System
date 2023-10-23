5666129790f1fe53b0e20f8bf2114980aef0733a
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-10-23 14:03:49 +0700

    Refactor data return for MaterialServiceImpl

--- FlowieeOfficial/src/main/java/com/flowiee/app/sanpham/services/impl/MaterialServiceImpl.java
+++ FlowieeOfficial/src/main/java/com/flowiee/app/sanpham/services/impl/MaterialServiceImpl.java
@@ -1,33 +1,52 @@
 package com.flowiee.app.sanpham.services.impl;
 
+import com.flowiee.app.common.utils.TagName;
 import com.flowiee.app.sanpham.entity.Material;
+import com.flowiee.app.sanpham.repository.MaterialRepository;
 import com.flowiee.app.sanpham.services.MaterialService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class MaterialServiceImpl implements MaterialService {
+    @Autowired
+    private MaterialRepository materialRepository;
+
     @Override
     public List<Material> findAll() {
-        return null;
+        return materialRepository.findAll();
     }
 
     @Override
     public Material findById(Integer entityId) {
-        return null;
+        return materialRepository.findById(entityId).get();
     }
 
     @Override
     public String save(Material entity) {
-        return null;
+        materialRepository.save(entity);
+        return TagName.SERVICE_RESPONSE_SUCCESS;
     }
 
     @Override
     public String update(Material entity, Integer entityId) {
-        return null;
+        if (entity == null || entityId == null || entityId <= 0) {
+            return TagName.SERVICE_RESPONSE_FAIL;
+        }
+        entity.setId(entityId);
+        materialRepository.save(entity);
+        return TagName.SERVICE_RESPONSE_SUCCESS;
     }
 
     @Override
     public String delete(Integer entityId) {
-        return null;
+        if (entityId == null || entityId <= 0) {
+            return TagName.SERVICE_RESPONSE_FAIL;
+        }
+        Material material = this.findById(entityId);
+        materialRepository.deleteById(entityId);
+        return TagName.SERVICE_RESPONSE_SUCCESS;
     }
 }
