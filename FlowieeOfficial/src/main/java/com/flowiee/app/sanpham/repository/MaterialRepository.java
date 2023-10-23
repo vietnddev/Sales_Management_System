94b699401da881510599a93ff69f6470d50333da
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-10-23 17:30:01 +0700

    Update MaterialRepository
    Add method findByCode(String code)

--- FlowieeOfficial/src/main/java/com/flowiee/app/sanpham/repository/MaterialRepository.java
+++ FlowieeOfficial/src/main/java/com/flowiee/app/sanpham/repository/MaterialRepository.java
@@ -2,8 +2,13 @@ package com.flowiee.app.sanpham.repository;
 
 import com.flowiee.app.sanpham.entity.Material;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MaterialRepository extends JpaRepository<Material, Integer> {
+    @Query("from Material m where m.code=:code")
+    List<Material> findByCode(String code);
 }
