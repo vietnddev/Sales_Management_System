46e645193d120a0102f8578fb85d3d3e60f744dd
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-10-23 17:32:13 +0700

    Update MaterialService
    Add method findByCode(String code)

--- FlowieeOfficial/src/main/java/com/flowiee/app/sanpham/services/MaterialService.java
+++ FlowieeOfficial/src/main/java/com/flowiee/app/sanpham/services/MaterialService.java
@@ -3,5 +3,8 @@ package com.flowiee.app.sanpham.services;
 import com.flowiee.app.base.BaseService;
 import com.flowiee.app.sanpham.entity.Material;
 
+import java.util.List;
+
 public interface MaterialService extends BaseService<Material> {
+    List<Material> findByCode(String code);
 }
