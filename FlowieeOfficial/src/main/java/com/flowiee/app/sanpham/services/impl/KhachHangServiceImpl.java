b128a796911bd1469abce8f08b221eb34a4afd82
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-10-23 14:02:48 +0700

    Refactor data return for KhachHangServiceImpl

--- FlowieeOfficial/src/main/java/com/flowiee/app/sanpham/services/impl/KhachHangServiceImpl.java
+++ FlowieeOfficial/src/main/java/com/flowiee/app/sanpham/services/impl/KhachHangServiceImpl.java
@@ -3,6 +3,7 @@ package com.flowiee.app.sanpham.services.impl;
 import com.flowiee.app.common.utils.FlowieeUtil;
 import com.flowiee.app.common.action.KhachHangAction;
 import com.flowiee.app.common.module.SystemModule;
+import com.flowiee.app.common.utils.TagName;
 import com.flowiee.app.hethong.service.AccountService;
 import com.flowiee.app.hethong.service.SystemLogService;
 import com.flowiee.app.sanpham.entity.KhachHang;
@@ -40,36 +41,36 @@ public class KhachHangServiceImpl implements KhachHangService {
     @Override
     public String save(KhachHang khachHang) {
         if (khachHang == null) {
-            return "NOK";
+            return TagName.SERVICE_RESPONSE_FAIL;
         }
         khachHang.setCreatedBy(FlowieeUtil.ACCOUNT_ID + "");
         khachHangRepository.save(khachHang);
         systemLogService.writeLog(module, KhachHangAction.CREATE_KHACHHANG.name(), "Thêm mới khách hàng: " + khachHang.toString());
         logger.info(SanPhamServiceImpl.class.getName() + ": Thêm mới khách hàng " + khachHang.toString());
-        return "OK";
+        return TagName.SERVICE_RESPONSE_SUCCESS;
     }
 
     @Override
     public String update(KhachHang khachHang, Integer id) {
-        if (id <=0 || this.findById(id) == null) {
-            return "NOK";
+        if (khachHang == null || id <= 0 || this.findById(id) == null) {
+            return TagName.SERVICE_RESPONSE_FAIL;
         }
         khachHang.setId(id);
         khachHangRepository.save(khachHang);
         systemLogService.writeLog(module, KhachHangAction.UPDATE_KHACHHANG.name(), "Cập nhật thông tin khách hàng: " + khachHang.toString());
         logger.info(SanPhamServiceImpl.class.getName() + ": Cập nhật khách hàng " + khachHang.toString());
-        return "OK";
+        return TagName.SERVICE_RESPONSE_SUCCESS;
     }
 
     @Override
     public String delete(Integer id) {
         KhachHang khachHang = this.findById(id);
         if (id <= 0 || khachHang == null) {
-            return "NOK";
+            return TagName.SERVICE_RESPONSE_FAIL;
         }
         khachHangRepository.deleteById(id);
         systemLogService.writeLog(module, KhachHangAction.DELETE_KHACHHANG.name(), "Xóa khách hàng: " + khachHang.toString());
         logger.info(SanPhamServiceImpl.class.getName() + ": Xóa khách hàng " + khachHang.toString());
-        return "OK";
+        return TagName.SERVICE_RESPONSE_SUCCESS;
     }
 }
