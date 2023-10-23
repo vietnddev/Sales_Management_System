471c7cae71bbd3e6aec6be79e33403725eeaffd8
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-10-23 14:06:13 +0700

    Add interface FabricTypeService

---
+++ FlowieeOfficial/src/main/java/com/flowiee/app/danhmuc/service/FabricTypeService.java
@@ -0,0 +1,7 @@
+package com.flowiee.app.danhmuc.service;
+
+import com.flowiee.app.base.BaseService;
+import com.flowiee.app.danhmuc.entity.FabricType;
+
+public interface FabricTypeService extends BaseService<FabricType> {
+}
