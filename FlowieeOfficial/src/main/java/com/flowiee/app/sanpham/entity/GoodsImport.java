7f05427fc164be84bce5e74dffb2a25f54fb81ba
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-10-24 17:33:01 +0700

    Add GoodsImport entity

---
+++ FlowieeOfficial/src/main/java/com/flowiee/app/sanpham/entity/GoodsImport.java
@@ -0,0 +1,64 @@
+package com.flowiee.app.sanpham.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.flowiee.app.base.BaseEntity;
+import com.flowiee.app.danhmuc.entity.HinhThucThanhToan;
+import com.flowiee.app.hethong.entity.Account;
+import com.flowiee.app.storage.entity.Material;
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "goods_import")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class GoodsImport extends BaseEntity implements Serializable {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "material_id")
+    private Material material;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "supplier_id")
+    private Supplier supplier;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @Column(name = "unit_price")
+    private Float unitPrice;
+
+    @Column(name = "discount")
+    private Float discount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "payment_method")
+    private HinhThucThanhToan paymentMethod;
+
+    @Column(name = "paidAmount")
+    private Float paidAmount;
+
+    @Column(name = "pay_status")
+    private String paidStatus;
+
+    @Column(name = "order_time")
+    private Date orderTime;
+
+    @Column(name = "received_time")
+    private Date receivedTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "received_by")
+    private Account receivedBy;
+
+    @Column(name = "note")
+    private String note;
+
+    @Column(name = "status", nullable = false)
+    private String status;
+}
