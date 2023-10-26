35dc6220ee0a96b8d6486cd0cedcc813f26c4d44
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-10-26 14:49:29 +0700

    Refactor code

--- FlowieeOfficial/src/main/java/com/flowiee/app/sanpham/repository/ProductVariantImportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.flowiee.app.sanpham.repository;
-
-import com.flowiee.app.sanpham.entity.SanPhamBienTheImport;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductVariantImportRepository extends JpaRepository<SanPhamBienTheImport, Integer> {
-}
