9b38fcd8b4900b06b0ae25a3ffe0f778bb5c9e26
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-11-10 09:40:31 +0700

    Build class BaseController general used for controllers

---
+++ FlowieeOfficial/src/main/java/com/flowiee/app/base/BaseController.java
@@ -0,0 +1,19 @@
+package com.flowiee.app.base;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.flowiee.app.common.utils.FlowieeUtil;
+import com.flowiee.app.system.service.NotificationService;
+
+@Component
+public class BaseController {
+	@Autowired
+	private NotificationService notificationService;
+	
+	public ModelAndView baseView(ModelAndView modelAndView) {
+		modelAndView.addObject("listNotification", notificationService.findAllByReceiveId(FlowieeUtil.ACCOUNT_ID));
+		return modelAndView;
+	}
+}
