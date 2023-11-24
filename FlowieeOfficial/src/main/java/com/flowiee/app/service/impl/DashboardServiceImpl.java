edfb9ad9a802179417a2ce9a80b6d463deb476a8
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-11-24 17:33:02 +0700

    BE - Refactor code dashboard

--- FlowieeOfficial/src/main/java/com/flowiee/app/service/impl/DashboardServiceImpl.java
+++ FlowieeOfficial/src/main/java/com/flowiee/app/service/impl/DashboardServiceImpl.java
@@ -8,6 +8,8 @@ import com.flowiee.app.model.product.DoanhThuTheoKenhBanHang;
 import com.flowiee.app.model.product.TopBestSeller;
 import com.flowiee.app.service.DashboardService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,17 +22,19 @@ import java.util.List;
 
 @Service
 public class DashboardServiceImpl implements DashboardService {
+    private static final Logger logger = LoggerFactory.getLogger(ProductVariantServiceImpl.class);
+
     @Autowired
     private EntityManager entityManager;
 
     @Override
     public DoanhThuTheoKenhBanHang getDoanhThuTheoKenhBanHang() {
-        StringBuilder query = new StringBuilder("SELECT c.TEN_LOAI, c.MAU_NHAN, NVL(SUM(d.TONG_TIEN_DON_HANG),0) AS TOTAL ");
-        query.append("FROM DM_KENH_BAN_HANG c ");
-        query.append("LEFT JOIN DON_HANG d ON c.ID = d.KENH_BAN_HANG ");
-        query.append("GROUP BY c.ten_loai, c.mau_nhan");
-
-        Query result = entityManager.createNativeQuery(query.toString());
+        StringBuilder strSQL = new StringBuilder("SELECT c.NAME, c.COLOR, NVL(SUM(d.TONG_TIEN_DON_HANG),0) AS TOTAL ");
+        strSQL.append("FROM (SELECT * FROM CATEGORY WHERE TYPE = 'SALESCHANNEL') c ");
+        strSQL.append("LEFT JOIN PRO_DON_HANG d ON c.ID = d.KENH_BAN_HANG ");
+        strSQL.append("GROUP BY c.NAME, c.COLOR");
+        logger.info("[getDoanhThuTheoKenhBanHang() - SQL findData]: " + strSQL.toString());
+        Query result = entityManager.createNativeQuery(strSQL.toString());
         @SuppressWarnings("unchecked")
 		List<Object[]> listData = result.getResultList();
 
@@ -52,23 +56,23 @@ public class DashboardServiceImpl implements DashboardService {
 
     @Override
     public DoanhThuCacThangTheoNam getDoanhThuCacThangTheoNam() {
-        StringBuilder query = new StringBuilder("SELECT ");
-        query.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 1 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_1, ");
-        query.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 2 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_2, ");
-        query.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 3 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_3, ");
-        query.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 4 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_4,");
-        query.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 5 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_5, ");
-        query.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 6 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_6, ");
-        query.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 7 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_7, ");
-        query.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 8 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_8,");
-        query.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 9 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_9, ");
-        query.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 10 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_10, ");
-        query.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 11 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_11, ");
-        query.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 12 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_12 ");
-        query.append("FROM DON_HANG d ");
-        query.append("WHERE EXTRACT(YEAR FROM d.THOI_GIAN_DAT_HANG) = EXTRACT(YEAR FROM SYSDATE)");
-
-        Query result = entityManager.createNativeQuery(query.toString());
+        StringBuilder strSQL = new StringBuilder("SELECT ");
+        strSQL.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 1 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_1, ");
+        strSQL.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 2 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_2, ");
+        strSQL.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 3 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_3, ");
+        strSQL.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 4 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_4,");
+        strSQL.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 5 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_5, ");
+        strSQL.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 6 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_6, ");
+        strSQL.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 7 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_7, ");
+        strSQL.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 8 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_8,");
+        strSQL.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 9 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_9, ");
+        strSQL.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 10 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_10, ");
+        strSQL.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 11 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_11, ");
+        strSQL.append("SUM(CASE WHEN EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = 12 THEN d.TONG_TIEN_DON_HANG ELSE 0 END) AS THANG_12 ");
+        strSQL.append("FROM PRO_DON_HANG d ");
+        strSQL.append("WHERE EXTRACT(YEAR FROM d.THOI_GIAN_DAT_HANG) = EXTRACT(YEAR FROM SYSDATE)");
+        logger.info("[getDoanhThuCacThangTheoNam() - SQL findData]: " + strSQL.toString());
+        Query result = entityManager.createNativeQuery(strSQL.toString());
         @SuppressWarnings("unchecked")
 		List<Object[]> listData = result.getResultList();
 
@@ -90,18 +94,18 @@ public class DashboardServiceImpl implements DashboardService {
         // Lấy ngày kết thúc của tháng hiện tại
         LocalDate ngayKetThuc = LocalDate.now().with(TemporalAdjusters.lastDayOfMonth());
 
-        StringBuilder query = new StringBuilder("WITH all_dates AS (");
-        query.append("SELECT TO_DATE('" + ngayBatDau + "', 'YYYY-MM-DD') + LEVEL - 1 AS NGAY ");
-        query.append("FROM DUAL ");
-        query.append("CONNECT BY LEVEL <= TO_DATE('" + ngayKetThuc + "', 'YYYY-MM-DD') - TO_DATE('" + ngayBatDau + "', 'YYYY-MM-DD') + 1");
-        query.append(") ");
-        query.append("SELECT all_dates.NGAY, NVL(SUM(DON_HANG.TONG_TIEN_DON_HANG), 0) AS DOANH_THU ");
-        query.append("FROM all_dates ");
-        query.append("LEFT JOIN DON_HANG ON TRUNC(DON_HANG.THOI_GIAN_DAT_HANG) = all_dates.NGAY ");
-        query.append("GROUP BY all_dates.NGAY ");
-        query.append("ORDER BY all_dates.NGAY");
-
-        Query result = entityManager.createNativeQuery(query.toString());
+        StringBuilder strSQL = new StringBuilder("WITH all_dates AS (");
+        strSQL.append("SELECT TO_DATE('" + ngayBatDau + "', 'YYYY-MM-DD') + LEVEL - 1 AS NGAY ");
+        strSQL.append("FROM DUAL ");
+        strSQL.append("CONNECT BY LEVEL <= TO_DATE('" + ngayKetThuc + "', 'YYYY-MM-DD') - TO_DATE('" + ngayBatDau + "', 'YYYY-MM-DD') + 1");
+        strSQL.append(") ");
+        strSQL.append("SELECT all_dates.NGAY, NVL(SUM(PRO_DON_HANG.TONG_TIEN_DON_HANG), 0) AS DOANH_THU ");
+        strSQL.append("FROM all_dates ");
+        strSQL.append("LEFT JOIN PRO_DON_HANG ON TRUNC(PRO_DON_HANG.THOI_GIAN_DAT_HANG) = all_dates.NGAY ");
+        strSQL.append("GROUP BY all_dates.NGAY ");
+        strSQL.append("ORDER BY all_dates.NGAY");
+        logger.info("[getDoanhThuCacNgayTheoThang() - SQL findData]: " + strSQL.toString());
+        Query result = entityManager.createNativeQuery(strSQL.toString());
         @SuppressWarnings("unchecked")
 		List<Object[]> listData = result.getResultList();
 
@@ -125,18 +129,18 @@ public class DashboardServiceImpl implements DashboardService {
 
     @Override
     public TopBestSeller getTopSanPhamBanChay() {
-        StringBuilder query = new StringBuilder("SELECT * ");
-        query.append("FROM (");
-        query.append("SELECT s.TEN_BIEN_THE, NVL(SUM(d.SO_LUONG), 0) AS Total ");
-        query.append("FROM SAN_PHAM_BIEN_THE s ");
-        query.append("LEFT JOIN DON_HANG_CHI_TIET d ");
-        query.append("ON s.id = d.BIEN_THE_SAN_PHAM_ID ");
-        query.append("GROUP BY s.ID, s.TEN_BIEN_THE ");
-        query.append("ORDER BY total DESC");
-        query.append(") ");
-        query.append("WHERE ROWNUM <= 10");
-
-        Query result = entityManager.createNativeQuery(query.toString());
+        StringBuilder strSQL = new StringBuilder("SELECT * ");
+        strSQL.append("FROM (");
+        strSQL.append("SELECT s.VARIANT_NAME, NVL(SUM(d.SO_LUONG), 0) AS Total ");
+        strSQL.append("FROM PRO_PRODUCT_VARIANT s ");
+        strSQL.append("LEFT JOIN PRO_DON_HANG_CHI_TIET d ");
+        strSQL.append("ON s.id = d.BIEN_THE_SAN_PHAM_ID ");
+        strSQL.append("GROUP BY s.ID, s.VARIANT_NAME ");
+        strSQL.append("ORDER BY total DESC");
+        strSQL.append(") ");
+        strSQL.append("WHERE ROWNUM <= 10");
+        logger.info("[getTopSanPhamBanChay() - SQL findData]: " + strSQL.toString());
+        Query result = entityManager.createNativeQuery(strSQL.toString());
         @SuppressWarnings("unchecked")
 		List<Object[]> listData = result.getResultList();
 
@@ -158,8 +162,9 @@ public class DashboardServiceImpl implements DashboardService {
 //        String query = "SELECT NVL(COUNT(*), 0) FROM DON_HANG D WHERE TRUNC(D.THOI_GIAN_DAT_HANG) = TRUNC(SYSDATE)";
 //        Query result = entityManager.createNativeQuery(query);
 //        return Integer.parseInt(String.valueOf(result.getSingleResult()));
-        String query = "SELECT * FROM DON_HANG D WHERE TRUNC(D.THOI_GIAN_DAT_HANG) = TRUNC(SYSDATE)";
-        Query result = entityManager.createNativeQuery(query);
+        String strSQL = "SELECT * FROM PRO_DON_HANG D WHERE TRUNC(D.THOI_GIAN_DAT_HANG) = TRUNC(SYSDATE)";
+        logger.info("[getSoLuongDonHangHomNay() - SQL findData]: " + strSQL.toString());
+        Query result = entityManager.createNativeQuery(strSQL);
         @SuppressWarnings("unchecked")
 		List<Order> listReturn = result.getResultList();
         return listReturn;
@@ -167,15 +172,17 @@ public class DashboardServiceImpl implements DashboardService {
 
     @Override
     public Float getDoanhThuHomNay() {
-        String query = "SELECT NVL(SUM(d.tong_tien_don_hang), 0) FROM DON_HANG d WHERE TRUNC(d.THOI_GIAN_DAT_HANG) = TRUNC(SYSDATE)";
-        Query result = entityManager.createNativeQuery(query);
+        String strSQL = "SELECT NVL(SUM(d.tong_tien_don_hang), 0) FROM PRO_DON_HANG d WHERE TRUNC(d.THOI_GIAN_DAT_HANG) = TRUNC(SYSDATE)";
+        logger.info("[getDoanhThuHomNay() - SQL findData]: " + strSQL.toString());
+        Query result = entityManager.createNativeQuery(strSQL);
         return Float.parseFloat(String.valueOf(result.getSingleResult()));
     }
 
     @Override
     public Float getDoanhThuThangNay() {
-        String query = "SELECT NVL(SUM(d.TONG_TIEN_DON_HANG), 0) FROM DON_HANG d WHERE EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = EXTRACT(MONTH FROM SYSDATE)";
-        Query result = entityManager.createNativeQuery(query);
+        String strSQL = "SELECT NVL(SUM(d.TONG_TIEN_DON_HANG), 0) FROM PRO_DON_HANG d WHERE EXTRACT(MONTH FROM d.THOI_GIAN_DAT_HANG) = EXTRACT(MONTH FROM SYSDATE)";
+        logger.info("[getDoanhThuThangNay() - SQL findData]: " + strSQL.toString());
+        Query result = entityManager.createNativeQuery(strSQL);
         return Float.parseFloat(String.valueOf(result.getSingleResult()));
     }
 
@@ -184,8 +191,9 @@ public class DashboardServiceImpl implements DashboardService {
 //        String query = "SELECT NVL(COUNT(*), 0) FROM KHACH_HANG c WHERE EXTRACT(MONTH FROM c.CREATED_AT) = EXTRACT(MONTH FROM SYSDATE)";
 //        Query result = entityManager.createNativeQuery(query);
 //        return Integer.parseInt(String.valueOf(result.getSingleResult()));
-        String query = "SELECT * FROM KHACH_HANG c WHERE EXTRACT(MONTH FROM c.CREATED_AT) = EXTRACT(MONTH FROM SYSDATE)";
-        Query result = entityManager.createNativeQuery(query);
+        String strSQL = "SELECT * FROM PRO_CUSTOMER c WHERE EXTRACT(MONTH FROM c.CREATED_AT) = EXTRACT(MONTH FROM SYSDATE)";
+        logger.info("[getSoLuongKhachHangMoi() - SQL findData]: " + strSQL.toString());
+        Query result = entityManager.createNativeQuery(strSQL);
         @SuppressWarnings("unchecked")
 		List<Customer> listReturn = result.getResultList();
         return listReturn;
