e0ae66c34a14f5deadefa4f156f98fe66c001dad
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-11-24 17:34:46 +0700

    BE - Load data use native query

--- FlowieeOfficial/src/main/java/com/flowiee/app/service/impl/OrderServiceImpl.java
+++ FlowieeOfficial/src/main/java/com/flowiee/app/service/impl/OrderServiceImpl.java
@@ -1,11 +1,9 @@
 package com.flowiee.app.service.impl;
 
+import com.flowiee.app.entity.*;
+import com.flowiee.app.model.product.OrderResponse;
 import com.flowiee.app.utils.AppConstants;
 import com.flowiee.app.utils.FlowieeUtil;
-import com.flowiee.app.entity.Account;
-import com.flowiee.app.entity.Customer;
-import com.flowiee.app.entity.Order;
-import com.flowiee.app.entity.OrderDetail;
 import com.flowiee.app.model.product.OrderRequest;
 import com.flowiee.app.category.Category;
 import com.flowiee.app.model.system.DonHangAction;
@@ -27,6 +25,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.transaction.Transactional;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -55,6 +55,8 @@ public class OrderServiceImpl implements OrderService {
     private ItemsService itemsService;
     @Autowired
     private CustomerContactService customerContactService;
+    @Autowired
+    private EntityManager entityManager;
 
     @Override
     public List<Order> findAll() {
@@ -226,5 +228,47 @@ public class OrderServiceImpl implements OrderService {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("redirect:/don-hang");
         }
+    }
+
+    private List<OrderResponse> findData() {
+        List<OrderResponse> dataResponse = new ArrayList<>();
+        StringBuilder strSQL = new StringBuilder("SELECT ");
+        strSQL.append("o.ID as ORDER_ID_0, o.MA_DON_HANG as MA_DON_HANG_1, o.THOI_GIAN_DAT_HANG as ORDER_TIME_2, o.RECEIVER_ADDRESS as RECEIVER_ADDRESS_3,");
+        strSQL.append("o.RECEIVER_PHONE as RECEIVER_PHONE_4,o.RECEIVER_NAME as RECEIVER_NAME_5, c.ID as ORDERBY_ID_6, c.TEN_KHACH_HANG as ORDER_BY_NAME_7,");
+        strSQL.append("o.TONG_TIEN_DON_HANG as TOTAL_AMOUNT_8, sc.ID as SALES_CHANNEL_ID_9, sc.NAME as SALES_CHANNEL_NAME_10, o.GHI_CHU as NOTE_11, ");
+        strSQL.append("os.ID as ORDER_STATUS_ID_12, os.NAME as ORDER_STATUS_NAME_13, op.ID as ORDER_PAY_ID_14, op.PAYMENT_STATUS as ORDER_PAY_STATUS_15, ");
+        strSQL.append("pm.ID as PAYMENT_METHOD_ID_16, pm.NAME as PAYMENT_METHOD_NAME_17, acc.ID as CASHIER_ID_18, acc.HO_TEN as CASHIER_NAME_19, ");
+        strSQL.append("o.CREATED_BY as CREATED_BY_ID_20, o.CREATED_AT as CREATED_AT_21");
+        strSQL.append("FROM PRO_DON_HANG o ");
+        strSQL.append("LEFT JOIN PRO_CUSTOMER c ON c.ID = o.CUSTOMER_ID ");
+        strSQL.append("LEFT JOIN PRO_DON_HANG_THANH_TOAN op ON op.DON_HANG_ID = o.ID ");
+        strSQL.append("LEFT JOIN SYS_ACCOUNT acc ON acc.ID = op.CASHIER ");
+        strSQL.append("LEFT JOIN (SELECT * FROM CATEGORY WHERE TYPE = 'SALESCHANNEL') sc ON sc.ID = o.KENH_BAN_HANG ");
+        strSQL.append("LEFT JOIN (SELECT * FROM CATEGORY WHERE TYPE = 'ORDERSTATUS') os ON os.ID = o.TRANG_THAI_DON_HANG ");
+        strSQL.append("LEFT JOIN (SELECT * FROM CATEGORY WHERE TYPE = 'PAYMETHOD') pm ON pm.ID = op.HINH_THUC_THANH_TOAN");
+        logger.info("[SQL findData]: " + strSQL.toString());
+        Query query = entityManager.createNativeQuery(strSQL.toString());
+        List<Object[]> listData = query.getResultList();
+        for (Object[] data : listData) {
+            OrderResponse order = new OrderResponse();
+            order.setOrderId(Integer.parseInt(String.valueOf(data[0])));
+            order.setOrderCode(String.valueOf(data[1]));
+            order.setOrderTime(FlowieeUtil.convertStringToDate(String.valueOf(data[2])));
+            order.setReceiverAddress(String.valueOf(data[3]));
+            order.setReceiverPhone(String.valueOf(data[4]));
+            order.setReceiverName(String.valueOf(data[5]));
+            order.setOrderBy(new Customer(Integer.parseInt(String.valueOf(data[6])), String.valueOf(data[7])));
+            order.setTotalAmount(Double.parseDouble(String.valueOf(data[8])));
+            order.setSalesChannel(new Category(Integer.parseInt(String.valueOf(data[9])), String.valueOf(data[10])));
+            order.setNote(String.valueOf(data[11]));
+            order.setOrderStatus(new Category(Integer.parseInt(String.valueOf(data[12])), String.valueOf(data[13])));
+            order.setOrderPay(new OrderPay(Integer.parseInt(String.valueOf(data[14])), Boolean.parseBoolean(String.valueOf(data[15]))));
+            order.setPayMethod(new Category(Integer.parseInt(String.valueOf(data[16])), String.valueOf(data[17])));
+            order.setCashier(new Account(Integer.parseInt(String.valueOf(data[18])), null, String.valueOf(data[19])));
+            order.setCreatedBy(new Account(Integer.parseInt(String.valueOf(data[20]))));
+            order.setCreatedAt(FlowieeUtil.convertStringToDate(String.valueOf(data[21])));
+            dataResponse.add(order);
+        }
+        return dataResponse;
     }
 }
