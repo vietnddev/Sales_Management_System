dffd978ad18462c78a160f83761080bb8ea82ba5
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-11-26 01:03:41 +0700

    BE - Refactor app

--- FlowieeOfficial/src/main/java/com/flowiee/FlowieeOfficialApplication.java
+++ FlowieeOfficial/src/main/java/com/flowiee/FlowieeOfficialApplication.java
@@ -2,43 +2,27 @@ package com.flowiee;
 
 import com.flowiee.app.service.MailService;
 
+import com.flowiee.app.utils.FlowieeUtil;
+import org.hibernate.tool.schema.internal.exec.ScriptTargetOutputToFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @CrossOrigin
 @EnableScheduling
 @SpringBootApplication
 public class FlowieeOfficialApplication {
-    @Autowired
-    private MailService mailService;
+//    @Autowired
+//    private MailService mailService;
 
     public static void main(String[] args) {
 		SpringApplication.run(FlowieeOfficialApplication.class, args);
-
-//        int year = 2023; // Năm cần in danh sách tuần
-//
-//        LocalDate firstDayOfYear = LocalDate.of(year, 1, 1);
-//        while (firstDayOfYear.getDayOfWeek() != DayOfWeek.MONDAY) {
-//            firstDayOfYear = firstDayOfYear.plusDays(1);
-//        }
-//
-//        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-//        for (int i = 0; i < 52; i++) { // Duyệt qua 52 tuần của năm (giả sử năm có 52 tuần)
-//            LocalDate startOfWeek = firstDayOfYear.plusWeeks(i);
-//            LocalDate endOfWeek = startOfWeek.plusDays(6);
-//
-//            // Kiểm tra tuần thuộc tháng nào
-//            Month startMonth = startOfWeek.getMonth();
-//            Month endMonth = endOfWeek.getMonth();
-//            String monthName = startMonth == endMonth ? startMonth.toString() : startMonth.toString() + " - " + endMonth.toString();
-//
-//            System.out.println("Tuần " + (i + 1) + ": " + startOfWeek.format(dateFormatter)
-//                    + " - " + endOfWeek.format(dateFormatter)
-//                    + " (Thuộc tháng: " + monthName + ")");
-        //}
     }
 
     //Auto gửi email báo cáo doanh thu hàng ngày
