c7107371c5b1372ee0aec1203ecf3e2b1ce573cf
vietnddev <nguyenducviet.vietnd@gmail.com>
2023-11-29 17:31:46 +0700

    BE - Add serialVersionUID for exception

--- src/main/java/com/flowiee/app/exception/DataExistsException.java
+++ src/main/java/com/flowiee/app/exception/DataExistsException.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.CONFLICT)
 public class DataExistsException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
 	public DataExistsException(String message) {
 		super(message);
 	}
--- src/main/java/com/flowiee/app/exception/ForbiddenException.java
+++ src/main/java/com/flowiee/app/exception/ForbiddenException.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.FORBIDDEN)
 public class ForbiddenException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
 	public ForbiddenException(String message) {
 		super(message);
 	};
