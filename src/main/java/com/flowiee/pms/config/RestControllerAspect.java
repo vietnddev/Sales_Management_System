3de7f91242fbbdd2065b27eb07459200f1095175
vietd.nguyen <vietd.nguyen@tx-tech.com>
2024-10-04 15:00:57 +0700

    Build event logs

--- src/main/java/com/flowiee/pms/config/RestControllerAspect.java
+++ src/main/java/com/flowiee/pms/config/RestControllerAspect.java
@@ -1,46 +1,151 @@
 package com.flowiee.pms.config;
 
+import com.flowiee.pms.entity.system.EventLog;
+import com.flowiee.pms.repository.system.EventLogRepository;
+import com.flowiee.pms.utils.CommonUtils;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Arrays;
+import java.util.Enumeration;
 
 @Aspect
 @Component
+@RequiredArgsConstructor
 public class RestControllerAspect {
-    protected final Logger log = LoggerFactory.getLogger(this.getClass());
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    private final EventLogRepository eventLogRepository;
+    private ThreadLocal<RequestContext> mvRequestContext = ThreadLocal.withInitial(RequestContext::new); // Tạo ThreadLocal để lưu thông tin của request
 
-    @Before("execution(* com.flowiee.pms.controller.category.*.*(..))")
-    public void logBeforeCategoryControllerCall(JoinPoint joinPoint) {
+    @Getter
+    @Setter
+    public static class RequestContext {
+        private long requestId;
+        private long startTime;
+        private String username;
+        private String ip;
+    }
+
+    public RequestContext getRequestContext() {
+        return mvRequestContext.get();
+    }
+
+    @Before("execution(* com.flowiee.pms.controller.*.*.*(..))")
+    public void beforeCall(JoinPoint joinPoint) {
+        long startTime = System.currentTimeMillis();
         Object[] args = joinPoint.getArgs();
         log.info("AOP Before call system controller {} with arguments: {}", joinPoint, Arrays.toString(args));
+
+        //Save request info into db
+        Signature signature = joinPoint.getSignature();
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        EventLog eventLog = eventLogRepository.save(EventLog.builder()
+                .httpMethod(getHttpMethod(attributes))// Lấy tên HTTP method (GET, POST, etc.)
+                .processClass(signature.getDeclaringTypeName())
+                .processMethod(signature.getName())
+                .requestUrl(getRequestUrl(attributes))
+                .requestParam(getRequestParam(attributes))
+                .requestBody(getRequestBody(joinPoint))
+                .createdBy(CommonUtils.getUserPrincipal().getUsername())
+                .createdTime(LocalDateTime.ofInstant(Instant.ofEpochMilli(startTime), ZoneId.systemDefault()))
+                .ipAddress(CommonUtils.getUserPrincipal().getIp())
+                .application(CommonUtils.productID)
+                .build());
+
+        RequestContext lvRequestContext = mvRequestContext.get();
+        lvRequestContext.setRequestId(eventLog.getRequestId());
+        lvRequestContext.setStartTime(startTime);
+        lvRequestContext.setUsername(CommonUtils.getUserPrincipal().getUsername());
+        lvRequestContext.setIp(CommonUtils.getUserPrincipal().getIp());
+        mvRequestContext.set(lvRequestContext);
     }
 
-    @Before("execution(* com.flowiee.pms.controller.product.*.*(..))")
-    public void logBeforeProductControllerCall(JoinPoint joinPoint) {
-        Object[] args = joinPoint.getArgs();
-        log.info("AOP Before call system controller {} with arguments: {}", joinPoint, Arrays.toString(args));
+    @After("execution(* com.flowiee.pms.controller.*.*.*(..))")
+    public void afterCall(JoinPoint joinPoint) {
+        RequestContext lvRequestContext = mvRequestContext.get();
+        long duration = System.currentTimeMillis() - lvRequestContext.getStartTime();
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+        EventLog eventLog = eventLogRepository.find(lvRequestContext.getRequestId(),
+                null,//getHttpMethod(attributes)
+                null,//getRequestUrl(attributes)
+                null);//LocalDateTime.ofInstant(Instant.ofEpochMilli(lvRequestContext.getStartTime()), ZoneId.systemDefault())
+        if (eventLog != null) {
+            eventLog.setDuration(duration);
+            eventLogRepository.save(eventLog);
+        }
+        mvRequestContext.remove();
     }
 
-    @Before("execution(* com.flowiee.pms.controller.sales.*.*(..))")
-    public void logBeforeSalesControllerCall(JoinPoint joinPoint) {
-        Object[] args = joinPoint.getArgs();
-        log.info("AOP Before call sales controller {} with arguments: {}", joinPoint, Arrays.toString(args));
+    private String getHttpMethod(ServletRequestAttributes attributes) {
+        if (attributes != null) {
+            return attributes.getRequest().getMethod();
+        }
+        return null;
     }
 
-    @Before("execution(* com.flowiee.pms.controller.storage.*.*(..))")
-    public void logBeforeStorageControllerCall(JoinPoint joinPoint) {
-        Object[] args = joinPoint.getArgs();
-        log.info("AOP Before call storage controller {} with arguments: {}", joinPoint, Arrays.toString(args));
+    private String getRequestUrl(ServletRequestAttributes attributes) {
+        if (attributes != null) {
+            HttpServletRequest httpServletRequest = attributes.getRequest();
+            return httpServletRequest.getRequestURL().toString();
+        }
+        return null;
     }
 
-    @Before("execution(* com.flowiee.pms.controller.system.*.*(..))")
-    public void logBeforeSystemControllerCall(JoinPoint joinPoint) {
+    private String getRequestParam(ServletRequestAttributes attributes) {
+        if (attributes != null) {
+            HttpServletRequest request = attributes.getRequest();
+            Enumeration<String> parameterNames = request.getParameterNames();
+            StringBuilder params = new StringBuilder();
+            while (parameterNames.hasMoreElements()) {
+                String paramName = parameterNames.nextElement();
+                String paramValue = request.getParameter(paramName);
+                params.append(paramName).append("=").append(paramValue).append(", ");
+            }
+            String paramsStr = params.toString();
+            if (paramsStr.endsWith(", ")) {
+                paramsStr = paramsStr.substring(0, paramsStr.length() - 2);
+            }
+            return paramsStr;
+        }
+        return null;
+    }
+
+    private String getRequestBody(JoinPoint joinPoint) {
+        // Lấy ra chữ ký của phương thức (MethodSignature)
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        // Lấy danh sách tham số của phương thức
         Object[] args = joinPoint.getArgs();
-        log.info("AOP Before call system controller {} with arguments: {}", joinPoint, Arrays.toString(args));
+        // Lấy danh sách các annotation của từng tham số
+        Annotation[][] parameterAnnotations = method.getParameterAnnotations();
+        // Loop qua các tham số và kiểm tra xem có annotation @RequestBody không
+        for (int i = 0; i < args.length; i++) {
+            for (Annotation annotation : parameterAnnotations[i]) {
+                if (annotation instanceof RequestBody) {
+                    return args[i].toString();
+                }
+            }
+        }
+        return null;
     }
 }
---
+++ src/main/java/com/flowiee/pms/entity/system/EventLog.java
@@ -0,0 +1,59 @@
+package com.flowiee.pms.entity.system;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Builder
+@Entity
+@Table(name = "event_log")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class EventLog implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long requestId;
+
+    @Column(name = "http_method")
+    private String httpMethod;
+
+    @Column(name = "process_class")
+    private String processClass;
+
+    @Column(name = "process_method")
+    private String processMethod;
+
+    @Column(name = "request_url")
+    private String requestUrl;
+
+    @Column(name = "request_param")
+    private String requestParam;
+
+    @Column(name = "request_body")
+    private String requestBody;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "created_time")
+    private LocalDateTime createdTime;
+
+    @Column(name = "duration")
+    private Long duration;
+
+    @Column(name = "ip_address")
+    private String ipAddress;
+
+    @Column(name = "application")
+    private String application;
+
+    @Column(name = "entity")
+    private String entity;
+}
