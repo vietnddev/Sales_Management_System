f83f581af087a74dd54eacba15516d9c80350fdd
vietd.nguyen <vietd.nguyen@tx-tech.com>
2024-10-24 16:41:37 +0700

    Reset password

--- src/main/java/com/flowiee/pms/service/system/impl/SendMailServiceImpl.java
+++ src/main/java/com/flowiee/pms/service/system/impl/SendMailServiceImpl.java
@@ -1,20 +1,19 @@
 package com.flowiee.pms.service.system.impl;
 
-import com.flowiee.pms.exception.AppException;
 import com.flowiee.pms.service.BaseService;
 import com.flowiee.pms.service.system.SendMailService;
 
-import com.flowiee.pms.utils.constants.MessageCode;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
 
 @Service
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -23,20 +22,20 @@ public class SendMailServiceImpl extends BaseService implements SendMailService 
     JavaMailSender mvJavaMailSender;
 
     @Override
-    public String sendMail(String subject, String to, String body) {
-        if (ObjectUtils.isEmpty(subject) || ObjectUtils.isEmpty(to) || ObjectUtils.isEmpty(body)) {
-            throw new AppException("Invalid mail information!");
-        }
-        try {
-            MimeMessage mimeMessage = mvJavaMailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-            helper.setSubject(subject);
-            helper.setTo(to);
-            helper.setText(body, true);
-            mvJavaMailSender.send(mimeMessage);
-            return MessageCode.DELETE_SUCCESS.getDescription();
-        } catch (MessagingException ex) {
-            throw new AppException(ex);
-        }
+    public boolean sendMail(String subject, String to, String body) throws UnsupportedEncodingException, MessagingException {
+        Assert.isNull(subject, "Subject cannot be null!");
+        Assert.isNull(to, "Recipient cannot be null!");
+        Assert.isNull(body, "Content cannot be null!");
+
+        MimeMessage mimeMessage = mvJavaMailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true);
+
+        messageHelper.setSubject(subject);
+        messageHelper.setTo(to);
+        messageHelper.setText(body, true);
+
+        mvJavaMailSender.send(mimeMessage);
+
+        return true;
     }
 }
